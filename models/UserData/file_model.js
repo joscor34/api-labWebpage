57306e67fb9ad7ea43d50c57b3a84f6530d8f988
joscor34 <zbrm34@gmail.com>
2022-09-28 13:16:10 -0500

    Se creo el modelo para el archivo

---
+++ models/UserData/file_model.js
@@ -0,0 +1,55 @@
+const moongose = require('mongoose')
+const config = require('../config')
+
+var Schema = moongose.Schema
+
+var FileSchema = Schema({
+	_userId: {
+		type: Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	paper: {
+		type: Number,
+		required: true
+	},
+	authors: [{
+		autor: {
+			type: String,
+			required: true
+		},
+		country: {
+			type: String,
+			required: true
+		},
+		affilation: {
+			type: String,
+			required: true
+		},
+	}],
+	title: {
+		type: String,
+		required: true
+	},
+	submitted_time: {
+		type : Number  //Por revisar
+	},
+	last_update_time: {
+		type : Number  //Por revisar
+	},
+	coordinador: {
+		type: String,
+		required: false
+	},
+	keywords: [{
+		keyword: {
+			type: String,
+			required: true
+		}
+	}],
+	abstract: {
+		type: String,
+		required: true
+	}
+	
+})
