8c6355e72ea21edc3873f85bf6bed932bc9bb905
joscor34 <zbrm34@gmail.com>
2022-09-29 20:12:02 -0500

    Se agrego la ruta para login y registrer, ademas se agregó el middleware para protejer la ruta de subir archivo

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,13 @@
-const { Router } = require("express");
 'use strict'
 
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user')
+const customMdw = require('../middleware/custom_middelware')
 
-router.get('/subir-archivo', userController.uploadFile)
+router.post('/login', userController.login)
+router.post('/register', userController.register)
+
+router.get('/subir-archivo', customMdw.ensureAuthenticated ,userController.uploadFile)
 
 module.exports = router
