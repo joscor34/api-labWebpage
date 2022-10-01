3216e838089a2b4acd1497ad4d7d27cb1bd1af16
joscor34 <zbrm34@gmail.com>
2022-10-01 00:02:31 -0500

    Se agrego el metodo crear un nuevo proyecto

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,12 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user')
+const uploadPdfMdw = require('../middleware/file_middleware')
 const customMdw = require('../middleware/custom_middelware')
 
 router.post('/login', userController.login)
 router.post('/register', userController.register)
 
-router.get('/subir-archivo', customMdw.ensureAuthenticated ,userController.uploadFile)
+router.post('/subir-archivo', uploadPdfMdw.single('proyecto') ,customMdw.ensureAuthenticated ,userController.uploadFile)
 
 module.exports = router
