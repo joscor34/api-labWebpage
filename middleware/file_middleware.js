3cbff5f7bac9260dc0a5591ac6ab10f67d328431
joscor34 <zbrm34@gmail.com>
2022-10-01 00:06:37 -0500

    Se agregó el middleware que manejará los archivos pdf

---
+++ middleware/file_middleware.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'controllers/uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
+module.exports = upload
+
